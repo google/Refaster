b34e8017f89458474a75f75a12d33b0ac1fbf56e
Louis Wasserman <lowasser@google.com>
2014-08-11 12:19:43 -0700

    Move UWhileLoop to third_party Refaster ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=72407172

---
+++ src/main/com/google/errorprone/refaster/UWhileLoop.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2013 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.refaster;
+
+import com.google.auto.value.AutoValue;
+
+import com.sun.source.tree.TreeVisitor;
+import com.sun.source.tree.WhileLoopTree;
+import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.JCTree.JCWhileLoop;
+
+import javax.annotation.Nullable;
+
+/**
+ * A {@link UTree} representation of a {@link WhileLoopTree}.
+ * 
+ * @author lowasser@google.com (Louis Wasserman)
+ */
+@AutoValue
+public abstract class UWhileLoop implements UStatement, WhileLoopTree {
+  public static UWhileLoop create(UExpression condition, UStatement body) {
+    return new AutoValue_UWhileLoop(condition, body);
+  }
+
+  @Override
+  public abstract UExpression getCondition();
+
+  @Override
+  public abstract UStatement getStatement();
+
+  @Override
+  public JCWhileLoop inline(Inliner inliner) throws CouldNotResolveImportException {
+    return inliner.maker().WhileLoop(
+        getCondition().inline(inliner),
+        getStatement().inline(inliner));
+  }
+
+  @Override
+  @Nullable
+  public Unifier unify(JCTree target, @Nullable Unifier unifier) {
+    if (unifier != null && target instanceof JCWhileLoop) {
+      JCWhileLoop loop = (JCWhileLoop) target;
+      unifier = getCondition().unify(loop.getCondition(), unifier);
+      return getStatement().unify(loop.getStatement(), unifier);
+    }
+    return null;
+  }
+
+  @Override
+  public <R, D> R accept(TreeVisitor<R, D> visitor, D data) {
+    return visitor.visitWhileLoop(this, data);
+  }
+
+  @Override
+  public Kind getKind() {
+    return Kind.WHILE_LOOP;
+  }
+}
