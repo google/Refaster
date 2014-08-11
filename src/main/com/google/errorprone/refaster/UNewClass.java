081d551bfb1f7a61c2aadbb9315e6c969d8d5824
Louis Wasserman <lowasser@google.com>
2014-08-11 12:21:54 -0700

    Move UNewClass to Refaster third_party ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=72577290

---
+++ src/main/com/google/errorprone/refaster/UNewClass.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2013 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.refaster;
+
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableList;
+
+import com.sun.source.tree.NewClassTree;
+import com.sun.source.tree.TreeVisitor;
+import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.JCTree.JCExpression;
+import com.sun.tools.javac.tree.JCTree.JCNewClass;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+/**
+ * {@link UTree} version of {@link NewClassTree}, which represents a constructor invocation.
+ *
+ * @author lowasser@google.com (Louis Wasserman)
+ */
+@AutoValue
+public abstract class UNewClass extends UExpression implements NewClassTree {
+
+  public static UNewClass create(UExpression enclosingExpression,
+      List<? extends UExpression> typeArguments, UExpression identifier,
+      List<UExpression> arguments, @Nullable UClassDecl classBody) {
+    return new AutoValue_UNewClass(
+        enclosingExpression, ImmutableList.copyOf(typeArguments), identifier, 
+        ImmutableList.copyOf(arguments), classBody);
+  }
+
+  public static UNewClass create(
+      List<? extends UExpression> typeArguments, UExpression identifier, 
+      UExpression... arguments) {
+    return create(null, typeArguments, identifier, ImmutableList.copyOf(arguments), null);
+  }
+
+  public static UNewClass create(UExpression identifier, UExpression... arguments) {
+    return create(ImmutableList.<UExpression>of(), identifier, arguments);
+  }
+
+  @Override
+  @Nullable
+  public abstract UExpression getEnclosingExpression();
+
+  /**
+   * Note: these are not the type arguments to the class, but to the constructor, for
+   * those extremely rare constructors that look like e.g. {@code <E> Foo(E e)}, where
+   * the type parameter is for the constructor alone and not the class. 
+   */
+  @Override
+  public abstract List<UExpression> getTypeArguments();
+
+  @Override
+  public abstract UExpression getIdentifier();
+
+  @Override
+  public abstract List<UExpression> getArguments();
+  
+  @Override
+  @Nullable
+  public abstract UClassDecl getClassBody();
+
+  @Override
+  @Nullable
+  public Unifier unify(JCTree target, @Nullable Unifier unifier) {
+    if (unifier != null && target instanceof JCNewClass) {
+      JCNewClass newClass = (JCNewClass) target;
+      unifier = Unifier.unifyNullable(
+          unifier, getEnclosingExpression(), newClass.getEnclosingExpression());
+      unifier = Unifier.unifyList(unifier, getTypeArguments(), newClass.getTypeArguments());
+      unifier = getIdentifier().unify(newClass.getIdentifier(), unifier);
+      unifier = Unifier.unifyNullable(unifier, getClassBody(), newClass.getClassBody());
+      return Unifier.unifyList(unifier, getArguments(), newClass.getArguments());
+    }
+    return null;
+  }
+
+  @Override
+  public Kind getKind() {
+    return Kind.NEW_CLASS;
+  }
+
+  @Override
+  public <R, D> R accept(TreeVisitor<R, D> visitor, D data) {
+    return visitor.visitNewClass(this, data);
+  }
+
+  @Override
+  public JCNewClass inline(Inliner inliner) throws CouldNotResolveImportException {
+    return inliner.maker().NewClass(
+        (getEnclosingExpression() == null) ? null : getEnclosingExpression().inline(inliner),
+        inliner.<JCExpression, UExpression>inlineList(getTypeArguments()),
+        getIdentifier().inline(inliner),
+        inliner.<JCExpression, UExpression>inlineList(getArguments()),
+        (getClassBody() == null) ? null : getClassBody().inline(inliner));
+  }
+}
