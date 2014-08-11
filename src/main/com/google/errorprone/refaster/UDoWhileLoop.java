56611749554dc3188d6d84ac6cce84577847b444
Louis Wasserman <lowasser@google.com>
2014-08-11 12:21:16 -0700

    Move UDoWhileLoop to third_party Refaster ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=72409872

---
+++ src/main/com/google/errorprone/refaster/UDoWhileLoop.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2013 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.refaster;
+
+import com.google.auto.value.AutoValue;
+
+import com.sun.source.tree.DoWhileLoopTree;
+import com.sun.source.tree.TreeVisitor;
+import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.JCTree.JCDoWhileLoop;
+
+import javax.annotation.Nullable;
+
+/**
+ * A {@link UTree} representation of a {@link DoWhileLoopTree}.
+ *
+ * @author lowasser@google.com (Louis Wasserman)
+ */
+@AutoValue
+public abstract class UDoWhileLoop implements UStatement, DoWhileLoopTree {
+  public static UDoWhileLoop create(UStatement body, UExpression condition) {
+    return new AutoValue_UDoWhileLoop(body, condition);
+  }
+
+  @Override
+  public abstract UStatement getStatement();
+
+  @Override
+  public abstract UExpression getCondition();
+
+  @Override
+  @Nullable
+  public Unifier unify(JCTree target, @Nullable Unifier unifier) {
+    if (unifier != null && target instanceof JCDoWhileLoop) {
+      JCDoWhileLoop loop = (JCDoWhileLoop) target;
+      unifier = getStatement().unify(loop.getStatement(), unifier);
+      return getCondition().unify(loop.getCondition(), unifier);
+    }
+    return null;
+  }
+
+  @Override
+  public <R, D> R accept(TreeVisitor<R, D> visitor, D data) {
+    return visitor.visitDoWhileLoop(this, data);
+  }
+
+  @Override
+  public Kind getKind() {
+    return Kind.DO_WHILE_LOOP;
+  }
+
+  @Override
+  public JCDoWhileLoop inline(Inliner inliner) throws CouldNotResolveImportException {
+    return inliner.maker().DoLoop(getStatement().inline(inliner), getCondition().inline(inliner));
+  }
+}
