da97dd11a4f89ef2088fe51c348636973c3a16db
Louis Wasserman <lowasser@google.com>
2014-08-11 12:19:25 -0700

    Move UIf to third_party Refaster ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=72406702

---
+++ src/main/com/google/errorprone/refaster/UIf.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2013 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.refaster;
+
+import com.google.auto.value.AutoValue;
+
+import com.sun.source.tree.IfTree;
+import com.sun.source.tree.TreeVisitor;
+import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.JCTree.JCIf;
+
+import javax.annotation.Nullable;
+
+/**
+ * {@link UTree} representation of an {@link IfTree}.
+ * 
+ * @author lowasser@google.com (Louis Wasserman)
+ */
+@AutoValue
+public abstract class UIf implements UStatement, IfTree {
+  public static UIf create(
+      UExpression condition, UStatement thenStatement, UStatement elseStatement) {
+    return new AutoValue_UIf(condition, thenStatement, elseStatement);
+  }
+
+  @Override
+  public abstract UExpression getCondition();
+
+  @Override
+  public abstract UStatement getThenStatement();
+
+  @Override
+  @Nullable
+  public abstract UStatement getElseStatement();
+
+  @Override
+  public <R, D> R accept(TreeVisitor<R, D> visitor, D data) {
+    return visitor.visitIf(this, data);
+  }
+
+  @Override
+  public Kind getKind() {
+    return Kind.IF;
+  }
+
+  @Override
+  @Nullable
+  public Unifier unify(JCTree target, Unifier unifier) {
+    if (unifier != null && target instanceof JCIf) {
+      JCIf ifTree = (JCIf) target;
+      unifier = getCondition().unify(ifTree.getCondition(), unifier);
+      unifier = getThenStatement().unify(ifTree.getThenStatement(), unifier);
+      return Unifier.unifyNullable(unifier, getElseStatement(), ifTree.getElseStatement());
+    }
+    return null;
+  }
+
+  @Override
+  public JCIf inline(Inliner inliner) throws CouldNotResolveImportException {
+    return inliner.maker().If(
+        getCondition().inline(inliner),
+        getThenStatement().inline(inliner),
+        (getElseStatement() == null) ? null : getElseStatement().inline(inliner));
+  }
+}
