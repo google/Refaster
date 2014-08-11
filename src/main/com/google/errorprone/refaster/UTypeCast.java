c35d631892a741a157f80d2552ca462b9fdc63c2
Louis Wasserman <lowasser@google.com>
2014-08-11 12:16:39 -0700

    Move UTypeCast to third_party Refaster. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=72189220

---
+++ src/main/com/google/errorprone/refaster/UTypeCast.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2013 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.refaster;
+
+import com.google.auto.value.AutoValue;
+
+import com.sun.source.tree.TreeVisitor;
+import com.sun.source.tree.TypeCastTree;
+import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.JCTree.JCTypeCast;
+
+import javax.annotation.Nullable;
+
+/**
+ * {@link UTree} version of {@link TypeCastTree}.
+ *
+ * @author lowasser@google.com (Louis Wasserman)
+ */
+@AutoValue
+public abstract class UTypeCast extends UExpression implements TypeCastTree {
+  public static UTypeCast create(UTree<?> type, UExpression expression) {
+    return new AutoValue_UTypeCast(type, expression);
+  }
+
+  @Override
+  public abstract UTree<?> getType();
+
+  @Override
+  public abstract UExpression getExpression();
+
+  @Override
+  @Nullable
+  public Unifier unify(JCTree target, @Nullable Unifier unifier) {
+    if (unifier != null && target instanceof JCTypeCast) {
+      JCTypeCast cast = (JCTypeCast) target;
+      unifier = getType().unify(cast.getType(), unifier);
+      return getExpression().unify(cast.getExpression(), unifier);
+    }
+    return null;
+  }
+
+  @Override
+  public Kind getKind() {
+    return Kind.TYPE_CAST;
+  }
+
+  @Override
+  public <R, D> R accept(TreeVisitor<R, D> visitor, D data) {
+    return visitor.visitTypeCast(this, data);
+  }
+
+  @Override
+  public JCTypeCast inline(Inliner inliner) throws CouldNotResolveImportException {
+    return inliner.maker().TypeCast(getType().inline(inliner), getExpression().inline(inliner));
+  }
+}
